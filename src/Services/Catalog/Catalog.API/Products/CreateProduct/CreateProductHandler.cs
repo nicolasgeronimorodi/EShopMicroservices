42c66fa9569b5ce8f135a21f53dc47e9084d71c9
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-03-02 18:29:23 -0300

    feat: enhance create product with CQRS and request models
    - Add ´Carter´ package reference in
      ´BuildingBlocks.csproj´
    - Add ´CreateProductRequest´ and
      ´CreateProductResponse´ records in
      ´CreateProductEndpoint.cs´
    - Update ´CreateProductCommand´ in
      ´CreateProductHandler.cs´ to include ´Name´ and
      implement ´ICommand<CreateProductResult>´
    - Update ´CreateProductCommandHandler´ to implement
      ´ICommandHandler<CreateProductCommand,
      CreateProductResult>´
    - Implement product creation logic in
      ´CreateProductCommandHandler´ using ´Product´
      and return new ´CreateProductResult´

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,30 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(List<string> Category, string Description, string ImageFile, decimal Price)
-    : IRequest<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal  class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // Perform business logic
-        throw new NotImplementedException();
+        // Create product entity
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // Save to database 
+        //...
+        // Return CreateProductResult
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
 
