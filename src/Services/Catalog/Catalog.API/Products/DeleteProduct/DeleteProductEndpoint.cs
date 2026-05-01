4a311d0db6d7f9f2bde0daf9dc654ad1bdf74c25
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-05-01 19:14:34 -0300

    fix: correct delete product endpoint route and command usage
    - update route in `DeleteProductEndpoint` to `/products/{id}`
    - replace `DeleteProductRequest` with `DeleteProductCommand`
    - comment out unused `DeleteProductRequest` record

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -3,17 +3,17 @@ using Catalog.API.Products.UpdateProduct;
 
 namespace Catalog.API.Products.DeleteProduct;
 
-public record DeleteProductRequest (Guid Id);
+// public record DeleteProductRequest (Guid Id);
 
 public record DeleteProductResponse(bool IsSuccess);
 public class DeleteProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapDelete("/products",
+        app.MapDelete("/products/{id}",
             async (Guid Id, ISender sender) =>
             {
-                var result = await sender.Send(new DeleteProductRequest(Id));
+                var result = await sender.Send(new DeleteProductCommand(Id));
                 var response = result.Adapt<DeleteProductResponse>();
                 return Results.Ok(response);
             })
