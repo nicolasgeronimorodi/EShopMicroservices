72edf4e94f50c44a7f14803f05b8c3aeae5a4392
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-06-15 21:31:07 -0300

    feat: add Basket store and delete operations
    - add `DeleteBasket` endpoint implementation
    - add `DeleteBasketCommand` and handler implementation
    - add `StoreBasket` endpoint implementation
    - add `StoreBasketCommand` and handler implementation
    - add request and response models for basket operations

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,5 +1,13 @@
-﻿namespace Basket.API.Basket.DeleteBasket;
+﻿
+namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+public class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
 {
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // await repository.DeleteBasket(command.UserName, cancellationToken)
+        return new DeleteBasketResult(true);
+    }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,15 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿
+namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+        //TODO: await repository.StoreBasket(command.Cart, cancellationToken);
+        return new StoreBasketResult(command.Cart.UserName);
+
+    }
 }
