73883b4298f263b8aeb43ab337f30f7957ca6ba4
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-06-21 01:44:16 -0300

    feat: add basket repository with Marten persistence
    - add `Marten` package reference to `Basket.API`
    - add `IBasketRepository` contract
    - add `BasketRepository` implementation using
      `IDocumentSession`
    - configure `Marten` storage for `ShoppingCart`
    - add `BasketNotFoundException`
    - add database connection string configuration
    - add exception and Marten global usings

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -10,6 +10,14 @@ builder.Services.AddMediatR(config =>
 
 });
 
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+
+}).UseLightweightSessions();
+
+
 var app = builder.Build();
 
 app.MapCarter();
