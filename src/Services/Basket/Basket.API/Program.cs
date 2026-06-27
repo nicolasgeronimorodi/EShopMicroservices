509bd2e4135869aab5096f11ca9c1c48204c8e94
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-06-26 21:04:17 -0300

    fix: use cached basket repository decorator
    - decorate `IBasketRepository` with
      `CachedBasketRepository`
    - update Redis container port mapping in
      `docker-compose.override.yml`

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddMarten(opts =>
 
 }).UseLightweightSessions();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
-builder.Services.Decorate<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
