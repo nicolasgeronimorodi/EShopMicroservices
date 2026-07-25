06160fda73b9efdb13c371ebe9a6d431e9e04c9a
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-07-25 14:34:48 -0300

    feat: add ordering domain base abstractions
    - Add `Aggregate<TId>` and `IAggregate` abstractions to support
      domain events
    - Add `Entity<T>` and `IEntity` abstractions with audit
      properties
    - Add `IDomainEvent` interface integrating with `MediatR`
    - Add `MediatR` package reference to
      `Ordering.Domain.csproj`

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions;
+public interface IDomainEvent : INotification
+{
+    Guid EventId => Guid.NewGuid();
+    public DateTime OccuredOn => DateTime.UtcNow;
+    public string EventTpe => GetType().AssemblyQualifiedName;
+
+
+}
