c5e41871e8952b1ae07ee1454c4d0aeda0f13054
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-08-09 21:02:47 -0300

    feat: add customer seed data to ordering database
    - Add ´InitialData´ with initial customer records
    - Update ´DatabaseExtensions´ to execute customer seeding
    - Add global usings for ordering domain models and value objects

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -9,5 +9,23 @@ public static class DatabaseExtensions
         using var scope = app.Services.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
         context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedAsync(context);
+    }
+
+    private static async Task SeedAsync(ApplicationDbContext context)
+    {
+        await SeedCustomerAsync(context);
+        //await SeedProductAsync(context);
+        //await SeedOrderAndItemsAsync(context);
+    }
+
+    private static async Task SeedCustomerAsync(ApplicationDbContext context)
+    {
+        if(!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
     }
 }
