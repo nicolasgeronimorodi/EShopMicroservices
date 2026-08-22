b789920ac1736eddc8a4020e50eebe26827b9dba
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-08-22 18:50:41 -0300

    feat: add application database context abstraction
    - Add ´IApplicationDbContext´ for database access in application layer
    - Expose customer, product, order, and order item sets
    - Add Entity Framework Core dependency to application project
    - Implement ´IApplicationDbContext´ in ´ApplicationDbContext´
    - Register ´IApplicationDbContext´ with dependency injection

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure;
 public static class DependencyInjection
@@ -20,7 +20,7 @@ public static class DependencyInjection
             options.AddInterceptors(serviceProvider.GetServices<ISaveChangesInterceptor>());
             options.UseSqlServer(connectionString);
         });
-        // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
         return services;
     }
 }
