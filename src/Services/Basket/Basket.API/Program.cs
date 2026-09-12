75251afe755cb55a2d49ced44ad2130acfaed837
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-09-12 17:58:32 -0300

    feat: configure message broker in Basket API
    - Add ´BuildingBlocks.Messaging´ project reference
    - Register ´AddMessageBroker´ in ´Program.cs´

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Exceptions.Handler;
+using BuildingBlocks.Messaging.MassTransit;
 using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -46,6 +47,8 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     return handler;
 });
 
+// Async communication services
+builder.Services.AddMessageBroker(builder.Configuration);
 
 //Cross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
